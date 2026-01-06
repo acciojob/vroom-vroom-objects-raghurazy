23a2938ec1f7b86cf0c2bbb24b7f0f58b96525e7
raghurazy <135259028+raghurazy@users.noreply.github.com>
2026-01-06 19:10:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 // Complete the js code
-function Car(make, model) {}
 
-function SportsCar(make, model, topSpeed) {}
+// Car constructor
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
+
+// Method on Car prototype
+Car.prototype.getMakeModel = function() {
+  return this.make + " " + this.model;
+};
+
+// SportsCar constructor
+function SportsCar(make, model, topSpeed) {
+  // Call the Car constructor to set make and model
+  Car.call(this, make, model);
+  this.topSpeed = topSpeed;
+}
+
+// Inherit from Car's prototype
+SportsCar.prototype = Object.create(Car.prototype);
+SportsCar.prototype.constructor = SportsCar;
+
+// Method on SportsCar prototype
+SportsCar.prototype.getTopSpeed = function() {
+  return this.topSpeed;
+};
 
 // Do not change the code below
 window.Car = Car;
